ca05f721d55cc4ff88afff0c1e18b0480646a72e
Matti Salokangas <matti.salokangas@gmail.com>
2015-11-12 07:23:18 -0800

    Generate image url using defaults

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-function RandomCat() {}
+function RandomCat() { }
 
-RandomCat.prototype.get = function () {
-  // @todo get a randcom cat picture.
-  return 'meow';
+RandomCat.prototype.get = function (options) {
+  return this._generateUrl(options);
+}
+
+RandomCat.prototype._generateUrl = function (options) {
+  options = options || {};
+
+  if (options.dummyText) {
+    options.dummyText = options.dummyText.replace(/ /g, '-');
+  }
+
+  var urlParts = [
+      options.width || 400
+    , options.height || 200
+    , options.category || 'cats'
+    , options.imageIndex
+    , options.dummyText
+  ];
+
+  var length = urlParts.length;
+  var protocol = options.protocol || 'http';
+  var baseUrl = options.baseUrl || 'lorempixel.com';
+  var lastSlash = baseUrl.lastIndexOf('/');
+  baseUrl = lastSlash === baseUrl.length - 1
+    ? baseUrl.substr(0, lastSlash)
+    : baseUrl;
+
+  for (var i=0; i<length; i++) {
+    if (!!!urlParts[i]) {
+      urlParts.splice(i, 1);
+    }
+  }
+
+  return protocol + '://' + baseUrl + '/' + urlParts.join('/');
 }
 
 RandomCat.prototype.constructor = RandomCat;
--- tests/index.js
+++ tests/index.js
@@ -4,9 +4,58 @@ var randomCat = require('..');
 var expect = require('expect.js');
 
 describe('random-cat', function () {
-  it('can get a random cat', function () {
-    var cat = randomCat.get();
+  it('can get a random cat with no options', function () {
+    var catUrl = randomCat.get();
 
-    expect(cat).to.not.be.null;
-  })
+    expect(catUrl).to.not.be.null;
+    expect(catUrl).to.match(/lorempixel.com/);
+    expect(catUrl).to.match(/http:\/\/lorempixel.com\/400\/200\/cats/);
+  });
+
+  it('can get a random cat with a width and height', function () {
+    var catUrl = randomCat.get({
+      width: 120,
+      height: 500
+    });
+
+    expect(catUrl).to.not.be.null;
+    expect(catUrl).to.match(/lorempixel.com/);
+    expect(catUrl).to.match(/http:\/\/lorempixel.com\/120\/500\/cats/);
+  });
+
+  it('can get a random picture with a category', function () {
+    var catUrl = randomCat.get({
+      width: 120,
+      height: 500,
+      category: 'people'
+    });
+
+    expect(catUrl).to.not.be.null;
+    expect(catUrl).to.match(/lorempixel.com/);
+    expect(catUrl).to.match(/http:\/\/lorempixel.com\/120\/500\/people/);
+  });
+
+  it('can get a random picture with an image index', function () {
+    var catUrl = randomCat.get({
+      width: 120,
+      height: 500,
+      imageIndex: '2'
+    });
+
+    expect(catUrl).to.not.be.null;
+    expect(catUrl).to.match(/lorempixel.com/);
+    expect(catUrl).to.match(/http:\/\/lorempixel.com\/120\/500\/cats\/2/);
+  });
+
+  it('can get a random picture with dummy text', function () {
+    var catUrl = randomCat.get({
+      width: 120,
+      height: 500,
+      dummyText: 'avocados are awesome'
+    });
+
+    expect(catUrl).to.not.be.null;
+    expect(catUrl).to.match(/lorempixel.com/);
+    expect(catUrl).to.match(/http:\/\/lorempixel.com\/120\/500\/cats\/avocados-are-awesome/);
+  });
 })
