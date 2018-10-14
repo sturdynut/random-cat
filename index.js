3d391e24c60debce04da16b23368842e15e765b7
Bharathvaj <bharathvaj1995@gmail.com>
2018-10-14 12:37:46 +0530

    update to es6

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-function RandomCat() { }
+const RANDOM_SIZES = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
 
-RandomCat.prototype.get = function (options) {
-  options = options || {};
+class RandomCat {
+	constructor(options = {}) {
+		this.gray = options.gray ? 'g' : false;
+		this.width = options.width || RANDOM_SIZES[Math.ceil(Math.random() * 12)];
+		this.height = options.height || RANDOM_SIZES[Math.ceil(Math.random() * 12)];
+		this.imageIndex = options.imageIndex;
+		this.message = options.message;
+	}
 
-  if (options.dummyText) {
-    options.dummyText = escape(options.dummyText);
-  }
-
-  var randomSizes = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
-
-  var urlParts = [
-      options.gray ? 'g' : false
-    , options.width || randomSizes[Math.ceil(Math.random() * 12)]
-    , options.height || randomSizes[Math.ceil(Math.random() * 12)]
-    , options.category || 'cats'
-    , options.imageIndex
-    , options.dummyText
-  ];
-
-  var length = urlParts.length;
-  var protocol = options.protocol || 'http';
-  var baseUrl = options.baseUrl || 'lorempixel.com';
-  var lastSlash = baseUrl.lastIndexOf('/');
-  baseUrl = lastSlash === baseUrl.length - 1
-    ? baseUrl.substr(0, lastSlash)
-    : baseUrl;
-
-  for (var i=0; i<length; i++) {
-    if (!!!urlParts[i]) {
-      urlParts.splice(i, 1);
-    }
-  }
-
-  return protocol + '://' + baseUrl + '/' + urlParts.join('/');
+	getURL() {
+		return `https://lorempixel.com/${this.width}/${this.height}/${this.category}/cats/${this.imageIndex}`;
+	}
 }
 
-module.exports = (new RandomCat());
+module.exports = RandomCat;
